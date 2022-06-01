b488564216b1b25411fc252033caadd3d4825975
yangfeng727 <15902316436@163.com>
2022-06-01 09:36:39 +0800

    '1'

--- myexpress/models/collectMongoDB.js
+++ myexpress/models/collectMongoDB.js
@@ -2,6 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var dburl = "mongodb://localhost:27017/"
 var {appLog} = require('../common/log');
 
+// 这里后面使用的话需要改造下 - 每次连接关闭数据库是不可取的，应该使用数据库连接池
 exports.collectMongoDB = function (callback) {
     MongoClient.connect(dburl, function(err, client) {
         if (err) {
